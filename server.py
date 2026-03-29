ae131eba30ab64a357845cb46c4d71181c939969
diivanak <dakong022@gmail.com>
2026-03-28 21:05:22 -0700

    update port number

--- server.py
+++ server.py
@@ -33,4 +33,4 @@ def get_weather():
 
 if __name__ == "__main__":
     #serve(app, host="0.0.0.0", port=8000)
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
